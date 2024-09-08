2e6ad8a758cdf8ee5b362e54fb2c5dd5545b08cc
HjorturDyr <druidfuns@gmail.com>
2024-09-08 12:53:23 +0000

    Hjörtur Innit commit

--- test/vibevault/src/main/java/vibevault/example/vibevault/VibevaultApplication.java
+++ test/vibevault/src/main/java/vibevault/example/vibevault/VibevaultApplication.java
@@ -3,6 +3,7 @@ package vibevault.example.vibevault;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//looks good
 @SpringBootApplication
 public class VibevaultApplication {
 
