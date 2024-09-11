d7491f9dfe89a61a66d92c34231a6b02c576ae4d
monsterblonster <36676480+monsterblonster@users.noreply.github.com>
2024-09-11 15:50:26 +0000

    Added dependencies from tech guide and modified the .gitignore as explained in the guide

---
+++ VibeVault/vibe/src/main/java/is/hi/hbv501g/vibe/VibeApplication.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501g.vibe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VibeApplication.class, args);
+	
+
+}
