11b59501218b85ded89285c6fe540379c30e994e
ODIN <odinnagust@protonmail.com>
2024-09-12 10:04:10 +0000

    breytti ehv til ad geta keyrt, vesen med driver og JAVA VERSION 21. vesen ad fa nyjasta fyrir mig

--- VibeVault/vibe/src/main/java/is/hi/hbv501g/vibe/VibeApplication.java
+++ VibeVault/vibe/src/main/java/is/hi/hbv501g/vibe/VibeApplication.java
@@ -2,12 +2,13 @@ package is.hi.hbv501g.vibe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class VibeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VibeApplication.class, args);
 	
-
+	}
 }
