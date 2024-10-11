a3630d537c3ec784465e9bc48d35408097e1eae5
JonOddr <joo31@hi.is>
2024-10-11 12:16:19 +0000

    Update EventRepository.java

--- VibeVault/vibe/src/main/java/is/hi/hbv501g/vibe/Persistance/Repositories/EventRepository.java
+++ VibeVault/vibe/src/main/java/is/hi/hbv501g/vibe/Persistance/Repositories/EventRepository.java
@@ -1,9 +1,17 @@
 package is.hi.hbv501g.vibe.Persistance.Repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import is.hi.hbv501g.vibe.Persistance.Entities.Event;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
+    Event save(Event event);
+    void delete(Event event);
+    List<Event> findAll();
+    List<Event> findByGroup(); // gæti þurft custom útfærslu
+    List<Event> findByName(String name);
+    Event findByID(long ID);
     
 }
