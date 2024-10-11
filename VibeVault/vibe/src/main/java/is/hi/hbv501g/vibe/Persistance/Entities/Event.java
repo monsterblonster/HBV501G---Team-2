b1e10f17eb731364c7d99c571d346b0ba0688bdb
JonOddr <joo31@hi.is>
2024-10-11 15:28:28 +0000

    Event.java getters, setters and constructor

--- VibeVault/vibe/src/main/java/is/hi/hbv501g/vibe/Persistance/Entities/Event.java
+++ VibeVault/vibe/src/main/java/is/hi/hbv501g/vibe/Persistance/Entities/Event.java
@@ -1,8 +1,8 @@
 package is.hi.hbv501g.vibe.Persistance.Entities;
 
+import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
-
-import is.hi.hbv501g.vibe.Persistance.Entities.User;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,9 +16,90 @@ public class Event {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long ID;
 
+    @Override
+    public String toString() {
+        return "Event [ID=" + ID + ", name=" + name + ", date=" + date + ", description=" + description + ", status="
+                + status + ", group=" + group + ", creator=" + creator + ", participants=" + participants
+                + ", comments=" + comments + "]";
+    }
+
+    protected Event(){}
+
+    public Event(String name, Date date, String description, String status, Group group, User creator){
+        this.name = name;
+        this.date = date;
+        this.description = description;
+        this.status = status;
+        this.group = group;
+        this.creator = creator;
+        List<User> particpant = new ArrayList<User>();
+        particpant.add(creator);
+        this.participants = particpant;
+        this.comments = new ArrayList<Comment>();
+    }
+    
     private String name;
+    private Date date;
+    private String description;
     private String status;
-    private User creator;
     private Group group;
+    private User creator;
     private List<User> participants;
+    private List<Comment> comments;
+    
+    public long getID() {
+        return ID;
+    }
+    public void setID(long iD) {
+        ID = iD;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Date getDate() {
+        return date;
+    }
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public Group getGroup() {
+        return group;
+    }
+    public void setGroup(Group group) {
+        this.group = group;
+    }
+    public User getCreator() {
+        return creator;
+    }
+    public void setCreator(User creator) {
+        this.creator = creator;
+    }
+    public List<User> getParticipants() {
+        return participants;
+    }
+    public void setParticipants(List<User> participants) {
+        this.participants = participants;
+    }
+    public List<Comment> getComments() {
+        return comments;
+    }
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+    
 }
