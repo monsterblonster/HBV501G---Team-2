c2bbdfe13e3a86897ef74d501e4ecaf028711910
HjorturDyr <druidfuns@gmail.com>
2024-10-18 12:48:26 +0000

    up2date

--- VibeVault/vibe/src/main/java/is/hi/hbv501g/vibe/Controllers/ChatController.java
+++ VibeVault/vibe/src/main/java/is/hi/hbv501g/vibe/Controllers/ChatController.java
@@ -1,16 +1,23 @@
 package is.hi.hbv501g.vibe.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import is.hi.hbv501g.vibe.Persistance.Entities.Comment;
 import is.hi.hbv501g.vibe.Services.CommentService;
+import is.hi.hbv501g.vibe.Services.GroupService;
+import is.hi.hbv501g.vibe.Services.UserService;
 
 @Controller
-@RequestMapping("/chats")
+@RequestMapping("/comments")
 public class ChatController {
 
-    @RequestMapping("/")
-    public String chatPage(){
-        return "chat";
+    private final CommentService commentService;
+
+
+    @Autowired
+    public ChatController(CommentService commentService) {
+        this.commentService = commentService;
     }
+    
 }
