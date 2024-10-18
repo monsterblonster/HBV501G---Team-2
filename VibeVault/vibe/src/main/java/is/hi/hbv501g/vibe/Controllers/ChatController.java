1312812689d753a5234dcdccc8456923a448813c
monsterblonster <binnistefansson@gmail.com>
2024-10-18 13:13:54 +0000

    comment/chat controller updated. Unsure of functionality. Needs HTML testing

--- VibeVault/vibe/src/main/java/is/hi/hbv501g/vibe/Controllers/ChatController.java
+++ VibeVault/vibe/src/main/java/is/hi/hbv501g/vibe/Controllers/ChatController.java
@@ -2,11 +2,14 @@ package is.hi.hbv501g.vibe.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import is.hi.hbv501g.vibe.Persistance.Entities.Comment;
 import is.hi.hbv501g.vibe.Services.CommentService;
+import org.springframework.ui.Model;
 import is.hi.hbv501g.vibe.Services.GroupService;
 import is.hi.hbv501g.vibe.Services.UserService;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/comments")
@@ -19,5 +22,18 @@ public class ChatController {
     public ChatController(CommentService commentService) {
         this.commentService = commentService;
     }
-    
+
+    @RequestMapping(method = RequestMethod.POST, value = "/comment")
+    public String commentViewPost(@ModelAttribute("comment") Comment comment, Model model){
+
+        commentService.save(comment);
+
+        model.addAttribute("comment", new Comment());
+
+        model.addAttribute("comment", commentService.findAll());
+
+        return "comment";
+    }
+
+
 }
