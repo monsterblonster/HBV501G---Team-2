eeaa255b4bb2ead477332d112db6bfa97cf851fd
ODIN <odinnagust@protonmail.com>
2024-11-12 23:09:34 +0000

    home page added!

--- VibeVault/vibe/src/main/java/is/hi/hbv501g/vibe/Controllers/HomeController.java
+++ VibeVault/vibe/src/main/java/is/hi/hbv501g/vibe/Controllers/HomeController.java
@@ -3,18 +3,14 @@ package is.hi.hbv501g.vibe.Controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 
 
-@RestController
+@Controller
+@RequestMapping("/")
 public class HomeController {
-	// ekki spurja ég hef ekki hugmynd, sleppa home page kóði
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public ResponseEntity<Void> homePage() {
-    return ResponseEntity.status(HttpStatus.FOUND)
-                         .header("Location", "/login") // breyta default landing page hér
-                         .build();
-} 
-
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String showHomePage() {
+        return "home"; 
+    }
 }
