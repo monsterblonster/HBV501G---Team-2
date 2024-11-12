92fa26d9e950f77e2c312474b7bfc13208dd376d
ODIN <odinnagust@protonmail.com>
2024-11-12 23:00:16 +0000

    Home controller redirect yfir i login page

--- VibeVault/vibe/src/main/java/is/hi/hbv501g/vibe/Controllers/HomeController.java
+++ VibeVault/vibe/src/main/java/is/hi/hbv501g/vibe/Controllers/HomeController.java
@@ -1,14 +1,20 @@
 package is.hi.hbv501g.vibe.Controllers;
 
-//import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+
 
 @RestController
 public class HomeController {
+	// ekki spurja ég hef ekki hugmynd, sleppa home page kóði
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ResponseEntity<Void> homePage() {
+    return ResponseEntity.status(HttpStatus.FOUND)
+                         .header("Location", "/login") // breyta default landing page hér
+                         .build();
+} 
 
-    @RequestMapping("/")
-    public String homePage(){
-        return "/login";
-    }
 }
